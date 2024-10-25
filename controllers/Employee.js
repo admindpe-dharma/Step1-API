a7f7b5fe4eece2ea69d35a0f92e76b2f5c559159
Franstio <franstiodiego01@gmail.com>
2024-10-26 00:33:06 +0700

    UPDATE: Additional Pending System UPDATE

--- controllers/Employee.js
+++ controllers/Employee.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 import employee from "../models/EmployeeModel.js";
 import container from "../models/ContainerModel.js";
 import bin from "../models/BinModel.js";
-import {io} from '../index.js';
+import { io } from '../index.js';
 import { response } from "express";
 import axios from "axios";
 import { Op, QueryTypes } from "sequelize";
@@ -15,19 +15,19 @@ import waste from "../models/WasteModel.js";
 import db from "../config/db.js";
 //import { updateBinWeightData } from "./Bin.js";
 
-const apiClient =  axios.create({
-        proxy:{
-            protocol:"http",
-            host : "10.77.8.70",
-            port:8080
-        }
-    });
+const apiClient = axios.create({
+    proxy: {
+        protocol: "http",
+        host: "10.77.8.70",
+        port: 8080
+    }
+});
 const syncApiClient = axios.create({
 
-    
+
 });
-syncApiClient.interceptors.response.use((res)=>res,
-(err)=>err);
+syncApiClient.interceptors.response.use((res) => res,
+    (err) => err);
 export const ScanBadgeid = async (req, res) => {
     const { badgeId } = req.body;
     try {
@@ -46,10 +46,10 @@ export const ScanBadgeid = async (req, res) => {
 export const ScanContainer = async (req, res) => {
     const { containerId } = req.body;
     try {
-        
+
 
         const container = await Container.findOne({
-            attributes: ['containerId', 'name', 'station', 'weightbin', 'IdWaste', 'type', 'line','hostname'],
+            attributes: ['containerId', 'name', 'station', 'weightbin', 'IdWaste', 'type', 'line', 'hostname'],
             include: [
                 {
                     model: Waste,
@@ -72,13 +72,13 @@ export const ScanContainer = async (req, res) => {
             where: { name: containerId }
         });
         const tr = await transaction.findOne({
-            where:{
+            where: {
                 idContainer: container.containerId,
                 status: "Waiting Dispose To Step 2"
             }
         });
         if (tr)
-            return res.status(409).json({error:"Id have already been used"});
+            return res.status(409).json({ error: "Id have already been used" });
         if (container) {
             res.json({ container: container });
         } else {
@@ -94,7 +94,7 @@ export const ScanMachine = async (req, res) => {
     const { machineId } = req.body;
     try {
         const machine = await Bin.findOne({
-            attributes: ['id', 'name','IdWaste','line'],
+            attributes: ['id', 'name', 'IdWaste', 'line'],
             include: [{
                 model: Waste,
                 as: 'waste',
@@ -102,15 +102,15 @@ export const ScanMachine = async (req, res) => {
                 duplicating: true,
                 foreignKey: 'IdWaste',
                 attributes: ['name'],
-            },{
-                    model: Container,
-                    as: 'container',
-                    required: true,
-                    duplicating: true,
-                    foreignKey: 'id',
-                    attributes: ['name','containerid','line']
+            }, {
+                model: Container,
+                as: 'container',
+                required: true,
+                duplicating: true,
+                foreignKey: 'id',
+                attributes: ['name', 'containerid', 'line']
             },
-        ],
+            ],
             where: { name: machineId }
         });
 
@@ -124,120 +124,111 @@ export const ScanMachine = async (req, res) => {
         res.status(500).json({ error: 'Terjadi kesalahan server' });
     }
 };
-export const syncPendingTransaction = async ()=>{
-    const pendingData = await db.query("Select t.Id,t.badgeId,c.name as containerName,c.hostname,w.name as wasteName,status,bin_qr,idscraplog,bin from transaction t inner join container c on t.idContainer=c.containerId inner join waste w on w.Id=t.IdWaste where t.status like '%PENDING%';",{type:QueryTypes.SELECT});
-    if (pendingData.length<1)
+export const syncPendingTransaction = async () => {
+    const pendingData = await db.query("Select t.Id,t.badgeId,c.name as containerName,c.hostname,w.name as wasteName,status,bin_qr,idscraplog,bin from transaction t inner join container c on t.idContainer=c.containerId inner join waste w on w.Id=t.IdWaste where t.status like '%PENDING%';", { type: QueryTypes.SELECT });
+    if (pendingData.length < 1)
         return pendingData;
-    for (let i=0;i<pendingData.length;i++)
-    {
+    for (let i = 0; i < pendingData.length; i++) {
         const error = pendingData[i].status.split('|');
-        error.splice(0,1);
+        error.splice(0, 1);
         const errorLoop = [...error];
-        for (let j=0;j<errorLoop.length;j++)
-        {
-            if (errorLoop[j] == 'PIDSG')
-            {
-                
+        for (let j = 0; j < errorLoop.length; j++) {
+            if (errorLoop[j] == 'PIDSG') {
+
                 const stationname = pendingData[i].containerName.split("-").slice(0, 3).join("-");
                 const result = await syncApiClient.post(
                     `http://${process.env.PIDSG}/api/pid/step1`,
                     {
-                      badgeno: pendingData[i].badgeId,
-                      logindate: "",
-                      stationname: stationname,
-                      frombinname: pendingData[i].bin_qr,
-                      tobinname: pendingData[i].containerName,
-                      weight: "0",
-                      activity: "Waiting Dispose To Step 2",
+                        badgeno: pendingData[i].badgeId,
+                        logindate: "",
+                        stationname: stationname,
+                        frombinname: pendingData[i].bin_qr,
+                        tobinname: pendingData[i].containerName,
+                        weight: "0",
+                        activity: "Waiting Dispose To Step 2",
                     },
                     {
-                        validateStatus: (status)=>true,
-                        timeout:1000,
-                        withCredentials:false
+                        validateStatus: (status) => true,
+                        timeout: 1000,
+                        withCredentials: false
                     }
                 );
-                if (result.status && result.status==200 && result.data.result && result.data.result != 'Fail' && result.data.result != null)
-                {
+                if (result.status && result.status == 200 && result.data.result && result.data.result != 'Fail' && result.data.result != null) {
                     const index = error.indexOf("PIDSG");
-                    if (index > -1)
-                    {
-                        error.splice(index,1);
+                    if (index > -1) {
+                        error.splice(index, 1);
                         pendingData[i].idscraplog = result.data.result;
                     }
-                } 
+                }
                 continue;
             }
-            else if (errorLoop[j] == "STEP2" && !error.includes("PIDSG") && pendingData[i].idscraplog  && pendingData[i].idscraplog != 'Fail' && pendingData[i].idscraplog != '')
-            {
-                const resStep2 = await  syncApiClient.post(`http://${pendingData[i].hostname}/Step1`,{
+            else if (errorLoop[j] == "STEP2" && !error.includes("PIDSG") && pendingData[i].idscraplog && pendingData[i].idscraplog != 'Fail' && pendingData[i].idscraplog != '') {
+                const resStep2 = await syncApiClient.post(`http://${pendingData[i].hostname}/Step1`, {
                     idscraplog: pendingData[i].idscraplog,
                     waste: pendingData[i].wasteName,
                     container: pendingData[i].containerName,
                     badgeId: pendingData[i].badgeId,
                     toBin: pendingData[i].bin
-                },{
-                    timeout:2000,
-                    withCredentials:false,
-                    validateStatus: (status)=>true
+                }, {
+                    timeout: 2000,
+                    withCredentials: false,
+                    validateStatus: (status) => true
                 });
-                if (resStep2.status && resStep2.status==200)
-                {
+                if (resStep2.status && resStep2.status == 200) {
                     const index = error.indexOf("STEP2");
-                    error.splice(index,1);
+                    error.splice(index, 1);
                 }
                 continue;
             }
         }
-        const newStatus = error.length  > 0 ? ["PENDING",...error] : ["Waiting Dispose To Step 2"];
+        const newStatus = error.length > 0 ? ["PENDING", ...error] : ["Waiting Dispose To Step 2"];
         pendingData[i].status = newStatus.join("|");
-        await db.query("Update Transaction set status=:newStatus where id=:id",{
+        await db.query("Update Transaction set status=:newStatus where id=:id", {
             type: QueryTypes.UPDATE,
-            replacements: {newStatus:newStatus.join("|"),id: pendingData[i].Id}
+            replacements: { newStatus: newStatus.join("|"), id: pendingData[i].Id }
         });
     }
 }
-export const syncTransaction = async (req, res)=>{
-    try
-    {
+export const syncTransaction = async (req, res) => {
+    try {
         const dataTransaction = await transaction.findAll({
-            include:[
+            include: [
                 {
-                    model:container,
-                    as:'container',
-                    required:true,
-                    foreignKey:'idContainer',
-                    where:{
+                    model: container,
+                    as: 'container',
+                    required: true,
+                    foreignKey: 'idContainer',
+                    where: {
                         station: {
-                            [Op.like] : '%'+req.params.hostname+'%'
+                            [Op.like]: '%' + req.params.hostname + '%'
                         }
                     }
                 },
                 {
-                    model:waste,
-                    as:"waste",
-                    foreignKey:"IdWaste",
-                    required:true,
+                    model: waste,
+                    as: "waste",
+                    foreignKey: "IdWaste",
+                    required: true,
                 }
             ],
-            where:{
-                status:'Waiting Dispose To Step 2',
-                idscraplog:{
+            where: {
+                status: 'Waiting Dispose To Step 2',
+                idscraplog: {
                     [Op.ne]: 'Fail'
                 }
             }
         });
         return res.status(200).json(dataTransaction ?? []);
     }
-    catch (err)
-    {
-        
-        res.status(500).json({ error: err});
+    catch (err) {
+
+        res.status(500).json({ error: err });
     }
 }
 export const getTransactionList = async (req, res) => {
     try {
         let response = await transaction.findAll({
-            attributes: ['id', 'badgeId', 'idContainer', 'idWaste', 'createdAt', 'status','bin_qr'],
+            attributes: ['id', 'badgeId', 'idContainer', 'idWaste', 'createdAt', 'status', 'bin_qr'],
             include: [{
                 model: Waste,
                 as: 'waste',
@@ -245,13 +236,13 @@ export const getTransactionList = async (req, res) => {
                 duplicating: true,
                 foreignKey: 'IdWaste',
                 attributes: ['name'],
-            },{
+            }, {
                 model: Container,
                 as: 'container',
                 required: true,
                 duplicating: true,
                 foreignKey: 'idContainer',
-                attributes: ['name','line']
+                attributes: ['name', 'line']
             },
             {
                 model: employee,
@@ -261,10 +252,10 @@ export const getTransactionList = async (req, res) => {
                 foreignKey: 'badgeId',
                 attributes: ['username']
             }],
-            order:[['createdAt','DESC']],
-            where:{
+            order: [['createdAt', 'DESC']],
+            where: {
                 idscraplog: {
-                    [Op.ne] : "Fail"
+                    [Op.ne]: "Fail"
                 }
             }
         });
@@ -335,19 +326,19 @@ export const CheckBinCapacity = async (req, res) => {
         res.status(500).json({ success: false, error: 'Internal Server Error' });
     }
 };
-export const checkTransaksi = async (req,res) =>{
-    const {idContainer,bin_qr,bin } = req.query;
-    
+export const checkTransaksi = async (req, res) => {
+    const { idContainer, bin_qr, bin } = req.query;
+
     const tr = await transaction.findOne({
-        where:{
+        where: {
             idContainer: idContainer,
             status: "Waiting Dispose To Step 2"
         }
     });
-    return tr ?  res.status(409).json({msg:"Transaction Already Registered"}) :res.status(200).json({msg:"OK"});
+    return tr ? res.status(409).json({ msg: "Transaction Already Registered" }) : res.status(200).json({ msg: "OK" });
 }
 export const SaveTransaksi = async (req, res) => {
-    const { payload ,error} = req.body;
+    const { payload, error } = req.body;
     //payload.recordDate = moment().format("YYYY-MM-DD HH:mm:ss");
     const _waste = await Waste.findOne({
         where: {
@@ -355,73 +346,75 @@ export const SaveTransaksi = async (req, res) => {
         }
     });
     const _container = await Container.findOne({
-        where:{
+        where: {
             containerId: payload.idContainer
         }
     });
     const tr = await transaction.findOne({
-        where:{
+        where: {
             idContainer: payload.idContainer,
             status: "Waiting Dispose To Step 2"
         }
     });
     if (tr)
-        return res.status(409).json({msg:"Transaction Already Registered"});
-    try
-    {
-    const _res =await  apiClient.post(`http://${_container.hostname}/Step1`,{
-        idscraplog: payload.idscraplog,
-        waste: _waste.name,
-        container: _container.name,
-        badgeId: payload.badgeId,
-        toBin: payload.bin
-    },{
-        validateStatus: (status)=>true
-    });
-    if (_res.status>= 300)
-        error.push('STEP2');
-    if (error && error.length > 0)
-        payload.status = ["PENDING",...error].join("|");
-    const latest = await transaction.create(payload);
-    await latest.save();
-    return res.status(200).json({ Id: latest.dataValues.Id ?? latest.dataValues.id });
+        return res.status(409).json({ msg: "Transaction Already Registered" });
+    try {
+        if (payload.idscraplog && payload.idscraplog != null) {
+            const _res = await apiClient.post(`http://${_container.hostname}/Step1`, {
+                idscraplog: payload.idscraplog,
+                waste: _waste.name,
+                container: _container.name,
+                badgeId: payload.badgeId,
+                toBin: payload.bin
+            }, {
+                validateStatus: (status) => true
+            });
+            if (_res.status >= 300)
+                error.push('STEP2');
+        }
+        else
+            error.push("STEP2");
+        if (error && error.length > 0)
+            payload.status = ["PENDING", ...error].join("|");
+        const latest = await transaction.create(payload);
+        await latest.save();
+        return res.status(200).json({ Id: latest.dataValues.Id ?? latest.dataValues.id });
     }
-    catch (err)
-    {
-        return res.status(500).json({error : (err.response ? err.response.data : err)});
+    catch (err) {
+        return res.status(500).json({ error: (err.response ? err.response.data : err) });
     }
 };
-export const DeleteTransaksi = async (req,res)=>{
-    const {id} = req.params.id;
-    const dataTransaction = await transaction.findOne({where:{
-        Id: id
-    }});
+export const DeleteTransaksi = async (req, res) => {
+    const { id } = req.params.id;
+    const dataTransaction = await transaction.findOne({
+        where: {
+            Id: id
+        }
+    });
     if (!dataTransaction)
-        return res.status(201).json({msg:'Data Not Found, No Issue'});
+        return res.status(201).json({ msg: 'Data Not Found, No Issue' });
     dataTransaction.destroy();
-    return res.status(200).json({msg:"ok"});
+    return res.status(200).json({ msg: "ok" });
 }
-export const UpdateTransaksi = async (req,res)=>{
-    const {idscraplog,logindate} = req.params;
-    const {status} = req.body;
+export const UpdateTransaksi = async (req, res) => {
+    const { idscraplog, logindate } = req.params;
+    const { status } = req.body;
     const _transaction = await transaction.findOne({
-        where:{
+        where: {
             idscraplog: idscraplog
         }
     });
     if (!_transaction)
-        return res.json({msg:"Transaction Not Found"},404);
-    try
-    {
-        await apiClient.put(`http://${process.env.PIDSG}/api/pid/step1/`+ idscraplog,{status:"Done",logindate:logindate});
-        _transaction.setDataValue("status",status);
+        return res.json({ msg: "Transaction Not Found" }, 404);
+    try {
+        await apiClient.put(`http://${process.env.PIDSG}/api/pid/step1/` + idscraplog, { status: "Done", logindate: logindate });
+        _transaction.setDataValue("status", status);
         //_transaction.setDataValue("neto",neto);
         await _transaction.save();
-        return res.json({msg:"Ok"},200);
+        return res.json({ msg: "Ok" }, 200);
     }
-    catch (err)
-    {
-        return res.json({error: err.response ? err.response.data :  err},500);
+    catch (err) {
+        return res.json({ error: err.response ? err.response.data : err }, 500);
     }
 }
 /*export const SaveTransaksiCollection = async (req, res) => {
@@ -516,26 +509,27 @@ export const UpdateLineContainer = async (req, res) => {
     }
 };
 
-export const UpdateStatus = async (req,res) => {
-    const {containerName,status,binQr} = req.body;   
-    const data = await  Container.findOne(
-        { where :{
-            name:containerName
-        }
-    });
-     await transaction.update({status:status},{
-        where:{
+export const UpdateStatus = async (req, res) => {
+    const { containerName, status, binQr } = req.body;
+    const data = await Container.findOne(
+        {
+            where: {
+                name: containerName
+            }
+        });
+    await transaction.update({ status: status }, {
+        where: {
             idContainer: data.containerId,
         }
     });
     io.emit("UpdateStep1");
-    res.status(200).json({msg:'ok'});
+    res.status(200).json({ msg: 'ok' });
 };
 
 export const getIdscaplog = async (req, res) => {
     try {
-        const {status, idContainer } = req.body;
-        
+        const { status, idContainer } = req.body;
+
         // Validasi input
         if (!status) {
             return res.status(400).json({ msg: "status harus disertakan" });
@@ -565,8 +559,8 @@ export const getIdscaplog = async (req, res) => {
 
 export const getIdmachine = async (req, res) => {
     try {
-        const {status, idContainer } = req.body;
-        
+        const { status, idContainer } = req.body;
+
         // Validasi input
         if (!status) {
             return res.status(400).json({ msg: "status harus disertakan" });
